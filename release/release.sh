5a98804d7748dc4febd4e9cbb549e44309ea6c0a
DuyAccel <nguyenchauhieuduy@outlook.com>
2024-11-19 21:10:02 +0700

    new/code-404: edit-commit-1

--- release/release.sh
+++ release/release.sh
@@ -2,7 +2,7 @@
 
 VERSION='new/code-404'
 
-sed -i -e "s|'{{VERSION}}'|${VERSION}|" overlays/production/alert/prometheus-rule.yaml
+sed -i -e "s|{{VERSION}}|${VERSION}|" overlays/production/alert/prometheus-rule.yaml
 
 kubectl apply -k overlays/production
 
