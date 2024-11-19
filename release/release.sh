7656b779adbbc8e5d4ad3a7a74ce9b61060ceb7a
DuyAccel <nguyenchauhieuduy@outlook.com>
2024-11-19 14:41:14 +0700

    new/code-404: edit structure

--- release/release.sh
+++ release/release.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-kubectl apply -k overlays/green
+VERSION='new/code-404'
+
+sed -i -e "s|'{{VERSION}}'|${VERSION}|" overlays/production/alert/prometheus-rule.yaml
+
+kubectl apply -k overlays/production
+
