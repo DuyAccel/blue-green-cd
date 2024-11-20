0650f4ccd3ecba4d2d62bb249b3a04aa209dac8e
DuyAccel <nguyenchauhieuduy@outlook.com>
2024-11-20 09:38:47 +0700

    new/code-404: edit alerts

--- release/release.sh
+++ release/release.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 VERSION='new/code-404'
+NEW_DEPLOY='green-demo'
+OLD_DEPLOY='demo'
 
-sed -i -e "s|{{VERSION}}|${VERSION}|" overlays/production/alert/prometheus-rule.yaml
+
+sed -i -e "s|{{VERSION}}|${VERSION}|" \
+  -e "s|{{NEW_DEPLOY}}|${NEW_DEPLOY}|" \
+  -e "s|{{OLD_DEPLOY}}|${OLD_DEPLOY}|" \
+  overlays/production/alert/prometheus-rule.yaml
 
 kubectl apply -k overlays/production
 
