58b613fbef5314063292a9893c77f6f047e802ff
DuyAccel <nguyenchauhieuduy@outlook.com>
2024-11-22 10:54:26 +0700

    main: edit routing.sh

---
+++ scripts/routing.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+###########################################
+#                                         #
+#    ROUTING TRAFFIC BETWEEN SERVICES     #
+#                                         #
+###########################################
+
+# === VARIABLE === #
+namespace=""
+route=""
+host=""
+port=""
+svc_main=""
+weight_main=1
+svc_alter=""
+weight_alter=0
+
+# === FUNCTION === #
+split_arguments() {
+  tmp=$(echo "$1" | tr ':' ' ')
+  echo "$tmp"
+}
+
+
+# === MAIN FUNCTION === #
+
+while [[ ${#} -gt 0 ]]; do 
+  case "$1" in
+    --svc-main) shift 
+      var=($(split_arguments "$1"))
+      svc_main="${var[0]}"
+      weight_main="${var[1]}"
+      ;;
+    --svc-alter) shift 
+      var=($(split_arguments "$1"))
+      svc_alter="${var[0]}"
+      weight_alter="${var[1]}"
+      ;;
+    --route) shift 
+      route="$1"
+      ;;
+    --host) shift 
+      host="$1"
+      ;;
+    --port) shift 
+      port="$1"
+      ;;
+    --namespace) shift 
+      namespace="$1"
+      ;;
+    *)
+      echo "Invalid parameter: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+if [[ $weight_alter -gt 0 ]]; then 
+  echo "\
+  alternateBackends:
+    - kind: Service
+      name: ${svc_alter}
+      weight: ${weight_alter}" > alter-backend.yaml
+else 
+  echo "" > alter-backend.yaml
+fi
+
+echo "$alter_backend"
+
+sed -e "s/{{ .Namespace }}/$namespace/" \
+    -e "s/{{ .Route }}/$route/" \
+    -e "s/{{ .Host }}/$host/" \
+    -e "s/{{ .Port }}/$port/" \
+    -e "s/{{ .Service }}/$svc_main/" \
+    -e "s/{{ .Weight }}/$weight_main/" \
+    -e "/{{ .Alter-backend }}/{
+        s/{{ .Alter-backend }}//
+        r alter-backend.yaml
+        }" route.tmpl > route.yaml
+
+cat route.yaml
+rm alter-backend.yaml
